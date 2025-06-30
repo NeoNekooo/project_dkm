e63cc61fb4b637e2ec2f225ba06e99d1a0522cf0
NeoNekooo <lgpplg7@gmail.com>
2025-06-30 14:30:05 +0700

    wlee

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/', [LoginController::class, 'formLogin']);
 Route::post('/login', [LoginController::class, 'loginAction'])->name('login.action');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
